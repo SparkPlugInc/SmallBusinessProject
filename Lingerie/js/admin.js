17cc71c9a10bd7a69134b09b552a523965682ca2
Lode Lesage <musilitar@gmail.com>
2014-10-14 15:24:48 +0200

    Admin work in progress

--- Lingerie/js/admin.js
+++ Lingerie/js/admin.js
@@ -1,7 +1,29 @@
+var APPLICATION_ID = 'A4D0E047-2158-8751-FF9A-29A946A7DF00',
+    SECRET_KEY = 'D693F3BE-40FC-03D4-FFD9-28CB2B4A2C00',
+    VERSION = 'v1';
+
+function Info(args) {
+    args = args || {};
+    this.email = args.email || "";
+    this.sex = args.sex || "";
+    this.favorite = args.favorite || "";
+}
+
 window.onload = function () {
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
     var database = Backendless.Persistence.of(Info);
 
+    var favorites = new Object();
+
     var countQuery = database.find(Info);
+    countQuery.data.forEach(function (info) {
+        if (!(info.favorite in favorites)) {
+            favorites[info.favorite] = 1;
+        } else {
+            favorites[info.favorite] += 1;
+        }
+    });
+
     var count = countQuery.data.length;
 
     var maleQuery = {
@@ -16,7 +38,7 @@ window.onload = function () {
     var females = database.find(femaleQuery);
     females = females.data.length;
 
-    document.getElementById("txtCount").innerText = count;
-    document.getElementById("txtMale").innerText = males;
-    document.getElementById("txtFemale").innerText = females;
+    document.getElementById("txtCount").innerText = "#People: " + count;
+    document.getElementById("txtMale").innerText = "#Males: " + males;
+    document.getElementById("txtFemale").innerText = "#Females: " + females;
 }
